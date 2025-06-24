b0d411232f124485cec67d2d72c9dc67620ffeda
Dhiraj Khandare <dhirajkhandare76@gmail.com>
2025-06-24 17:31:09 +0530

    Add files via upload

---
+++ FBS_JAVA_FULL_STACK/WhileLoop.java
@@ -0,0 +1,10 @@
+public class WhileLoop{
+	public static void main(String[] args){
+		System.out.println("This is While Loop: ");
+		int i=0;
+		while(i<=10){
+			System.out.println(i);
+			i+=1;
+}
+}
+}
